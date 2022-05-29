47e86a240585ac12aa6dec8586939591298590b9
lilachdaniel <lilachdaniel100@gmail.com>
2022-05-29 14:26:06 +0300

    terminates on over 1MB file, handled SIGINT

--- pcc_server.c
+++ pcc_server.c
@@ -15,12 +15,35 @@
 #include <sys/stat.h>
 #include <endian.h>
 #include <stdint.h>
+#include <signal.h>
+#include <stdatomic.h>
 
 #define MAX_BUF_SIZE 1000000 // == MB
 #define MIN_PRINT_CHAR 32
 #define MAX_PRINT_CHAR 126
 #define NUM_PRINT_CHARS 95
 
+atomic_int received_sigint;
+uint64_t *stats;
+
+void print_stats() {
+	int i;
+	char c;
+	
+	for (i = 0; i < NUM_PRINT_CHARS; ++i) {
+		c = (char)(i + MIN_PRINT_CHAR);
+		printf("char '%c' : %lu times\n", c, stats[i]);
+	}
+}
+
+void handle_sigint_in_handle_connection() {
+	received_sigint = 1;
+}
+
+void handle_sigint_in_main() {
+	print_stats();
+	exit(0);
+}
 
 int write_sock(int sock, char *buff, size_t count) {
 	int num_writ, curr_num_writ;
@@ -45,14 +68,15 @@ int write_sock(int sock, char *buff, size_t count) {
 }
 
 int read_sock(int sock, char *buff, size_t count) {
-	int num_read, curr_num_read;
+	long num_read, curr_num_read;
 	
 	num_read = 0;
-	printf("count = %d\n", (int)count);
+	printf("count = %ld\n", count);
     while (num_read < count) {
-    	printf("num_read = %d\n", num_read);
+    	printf("num_read = %ld\n", num_read);
+    	printf("left to read = %ld\n", count - num_read);
         curr_num_read = recv(sock, &buff[num_read], count - num_read, 0);
-        printf("curr_num_read = %d\n", curr_num_read);
+        printf("curr_num_read = %ld\n", curr_num_read);
         if (curr_num_read > 0) {
             num_read += curr_num_read;
         } 
@@ -64,32 +88,43 @@ int read_sock(int sock, char *buff, size_t count) {
             return -1;
         }
     }
-    
+    printf("read_sock returned %lu\n", num_read);
     return num_read;
 }
 
-int update_statistics(int sock, int *stats, uint64_t size_host) {
-	int new_stats[NUM_PRINT_CHARS] = {0};
+int update_statistics(int sock, uint64_t size_host) {
+	uint64_t new_stats[NUM_PRINT_CHARS] = {0};
     char buff[MAX_BUF_SIZE];
-    int num_bytes_recv, i, ind, num_printable = 0;
+    int curr_size_host, num_bytes_recv, i, ind, num_printable = 0;
 
-    num_bytes_recv = read_sock(sock, (char *)&buff, size_host);
-    if (num_bytes_recv < 0) {
-    	return -1;
-    }
+	while (size_host > 0) {
+		printf("size_host = %lu\n", size_host);
+		/* find size to pass to read_sock */
+		if (size_host > MAX_BUF_SIZE) {
+			curr_size_host = MAX_BUF_SIZE;
+		}
+		else {
+			curr_size_host = size_host;
+		}
+		
+		/* read socket */
+    	num_bytes_recv = read_sock(sock, (char *)&buff, curr_size_host);
+    	if (num_bytes_recv < 0) {
+    		return -1;
+    	}
 
-    /* update new statistics */
-    for (i = 0; i < num_bytes_recv; ++i) {
-        ind = buff[i];
-        if (ind >= MIN_PRINT_CHAR && ind <= MAX_PRINT_CHAR) { /* is printable! */
-            ind -= MIN_PRINT_CHAR;
-             new_stats[ind]++;
-           num_printable++;
-        }
+    	/* update new statistics */
+    	for (i = 0; i < num_bytes_recv; ++i) {
+        	ind = buff[i];
+        	if (ind >= MIN_PRINT_CHAR && ind <= MAX_PRINT_CHAR) { /* is printable! */
+            	ind -= MIN_PRINT_CHAR;
+             	new_stats[ind]++;
+           		num_printable++;
+        	}
+		}
+
+		size_host -= num_bytes_recv;
 	}
-    
-
-	size_host -= num_bytes_recv;
 
     /* update statistics */
     for (i = 0; i < NUM_PRINT_CHARS; ++i) {
@@ -100,8 +135,16 @@ int update_statistics(int sock, int *stats, uint64_t size_host) {
 }
 
 
-void handle_connection(int conn_sock, int *stats) {
+void handle_connection(int conn_sock) {
 	uint64_t size_net, size_host, num_printable_host, num_printable_net;
+	
+	/* handle SIGINT */
+	struct sigaction act = {.sa_handler = handle_sigint_in_handle_connection};
+	
+	if (sigaction(SIGINT, &act, NULL) == -1) {
+		perror(strerror(errno));
+		exit(1);
+	}
 	
 	/* receive size */
 	printf("receiving N...\n");
@@ -109,10 +152,11 @@ void handle_connection(int conn_sock, int *stats) {
 		return;
 	}
 	size_host = be64toh(size_net);
+	printf("size host = %lu\n", size_host);
 	
 	/* update statistics */
 	printf("updating statistics...\n");
-	num_printable_host = update_statistics(conn_sock, stats, size_host);
+	num_printable_host = update_statistics(conn_sock, size_host);
 	if (num_printable_host < 0) {
 		return;
 	}
@@ -129,7 +173,18 @@ void handle_connection(int conn_sock, int *stats) {
 int main(int argc, char *argv[]) {
 	int port, lis_sock, conn_sock;
 	struct sockaddr_in serv_addr = {0};
-	int *stats = (int *)calloc(MAX_BUF_SIZE, sizeof(int)); /* printable chars range from 32 to 126 */
+	
+	/* handle SIGINT */
+	struct sigaction act = {.sa_handler = handle_sigint_in_main};
+	
+	if (sigaction(SIGINT, &act, NULL) == -1) {
+		perror(strerror(errno));
+		exit(1);
+	}
+	
+	/* initialize global variables */
+	received_sigint = 0;
+	stats = (uint64_t *)calloc(MAX_BUF_SIZE, sizeof(uint64_t)); /* printable chars range from 32 to 126 */
 	
 	/* handle arguments */
 	if (argc != 2) {
@@ -164,6 +219,12 @@ int main(int argc, char *argv[]) {
     
     /* accept and handle connection */
     while (1) {
+    	/* handle sigint */
+    	if (sigaction(SIGINT, &act, NULL) == -1) {
+			perror(strerror(errno));
+			exit(1);
+		}
+	
     	/* accept connection */
     	printf("accepting connection...\n");
     	conn_sock = accept(lis_sock, NULL, NULL);
@@ -173,7 +234,11 @@ int main(int argc, char *argv[]) {
     	}
     	
     	/* handle connection */
-    	handle_connection(conn_sock, stats);
+    	handle_connection(conn_sock);
+    	
+    	if (received_sigint) {
+    		handle_sigint_in_main();
+    	}
     	
     	/* close up */
     	close(conn_sock);
